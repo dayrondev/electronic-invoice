8b3784bd5199d9cb7ff2515191e9f38faf10f772
dayrondev <dayrondev@gmail.com>
2025-02-11 11:13:00 +0100

    feature(frontend): add mode toogle

---
+++ frontend/src/components/mode-toggle.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+interface SimpleModeToggleProps {
+  className?: string;
+  size?: "default" | "sm" | "lg" | "icon";
+}
+
+export function ModeToggle({
+  size = "icon",
+  className,
+}: SimpleModeToggleProps) {
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <Button
+      variant="outline"
+      size={size}
+      onClick={toggleTheme}
+      className={cn(className)}
+    >
+      {theme === "dark" ? (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      )}
+    </Button>
+  );
+}
