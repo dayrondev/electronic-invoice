5557075193f8a159a388e458866e4bd833de746d
dayrondev <dayrondev@gmail.com>
2025-02-16 17:48:22 +0100

    feat(frontend): add product pages

---
+++ frontend/src/app/application/products/new/page.tsx
@@ -0,0 +1,3 @@
+export default function NewProduct() {
+  return <div className="flex flex-1 flex-col gap-4 p-4 pt-0">New Product</div>;
+}
---
+++ frontend/src/app/application/products/page.tsx
@@ -0,0 +1,3 @@
+export default function Products() {
+  return <div className="flex flex-1 flex-col gap-4 p-4 pt-0">Products</div>;
+}
