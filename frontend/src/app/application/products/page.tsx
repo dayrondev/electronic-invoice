85249ac678bcea07ff6ffa83a6ceedf1b2a59222
dayrondev <dayrondev@gmail.com>
2025-02-16 18:59:53 +0100

    feat(frontend): add functionality to show product list

--- frontend/src/app/application/products/page.tsx
+++ frontend/src/app/application/products/page.tsx
@@ -1,3 +1,16 @@
-export default function Products() {
-  return <div className="flex flex-1 flex-col gap-4 p-4 pt-0">Products</div>;
+import { getProductsByCompany } from "@/lib/company";
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
+
+export default async function DemoPage() {
+  const result = await getProductsByCompany(
+    "14c78837-941a-49b8-b752-6752ff572c8d"
+  );
+  if (!result.ok) return null;
+
+  return (
+    <div className="container mx-auto p-10 pt-0">
+      <DataTable columns={columns} data={result.data} />
+    </div>
+  );
 }
