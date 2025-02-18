7ea6214ff2e48dda1b7d9f82039e59a37299d61a
dayrondev <dayrondev@gmail.com>
2025-02-18 13:31:38 +0100

    feat(frontend): update styles

--- frontend/src/app/application/products/page.tsx
+++ frontend/src/app/application/products/page.tsx
@@ -31,7 +31,7 @@ export default function ProductsPage() {
     );
 
   return (
-    <div className="container mx-auto p-10 pt-0">
+    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
       <DataTable columns={columns} data={products} />
     </div>
   );
