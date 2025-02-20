4179c10b2165efa1b8634c5890b6743fac014e44
dayrondev <dayrondev@gmail.com>
2025-02-20 17:12:37 +0100

    feat(backend): add scraper service

--- backend/src/company/company.module.ts
+++ backend/src/company/company.module.ts
@@ -3,9 +3,10 @@ import { CompanyService } from './company.service';
 import { CompanyController } from './company.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ProductService } from 'src/product/product.service';
+import { ScraperService } from './scraper.service';
 
 @Module({
   controllers: [CompanyController],
-  providers: [CompanyService, PrismaService, ProductService],
+  providers: [CompanyService, PrismaService, ProductService, ScraperService],
 })
 export class CompanyModule {}
